8c99196bee085bdd9acb1d48f29511cd3bd0ab87
mguittari <mattias.guittari@gmail.com>
2024-12-07 17:15:39 +0100

    planned versions listed in the readme | added gun pic and barrel chambers

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# russian-roulette
+# russian-roulette algo game
+
+### V1
+
+- 1 joueur (même s'il est techniquement possible de jouer à plusieurs)
+- Barillet à 6 emplacements
+- 1 seule balle dans le barillet
+- Au clique du bouton, une nouvelle phrase apparait
+- Ronds remplis ou vides qui symbolisent l'état actuel du barillet et se mettent à jour à chaque clique
+- Le joueur clique jusqu'à mourir
+- Un émoji explosion apparait pendant 1 fraction de seconde au moment de la mort
+- "Vous êtes mort" s'affiche dans un style graphique Dark Souls
+- Bouton relancer le jeu
+
+### V2
+
+- 2 à 5 joueurs (choix)
+- Les joueurs sont invités à cliquer à tour de rôle
+- À la mort d'un joueur possibilité de relancer le jeu
+
+### V3
+
+- À partir de 3 joueurs, après chaque mort le jeu continue jusqu'à ce qu'il n'en reste qu'un
+
+### V4
+
+- Choix entre plusieurs taille de barillet (6 - 7 - 8 - 9)
+- Possibilité de mettre plus d'une balle dans le barillet (max = capacité max du barillet - 1)
---
+++ script.js
@@ -0,0 +1,52 @@
+const quotes = [
+  "Pour finir, une citation de Jankélévitch : 'La mort, c’est ce qui met fin à tout, même à la possibilité de la parole.' La dernière chambre du barillet contient la balle. Pose ce revolver et reviens avec tes amis pour la V2.",
+  "Montaigne n'a-t-il pas écrit : 'La mort, c’est l’oubli où l’on est plongé dans le néant, sans espoir de retour.' Plus tu avances dans le jeu et plus l'espoir de retour se fait mince... Veux-tu vraiment continuer ?",
+  "'La mort est notre plus grande ennemie. Mais il faut la regarder en face, car c’est dans le regard de la mort que l’homme apprend à vivre.' Cette citation provient de Nietzsche, dans Ainsi parlait Zarathoustra. Nietzsche aurait-il aimé joué à ce jeu ? Clique sur le bouton pour lui demander...",
+  "Épicure dans la lettre à Ménécée : 'La mort n’est rien pour nous, puisque, tant que nous existons, la mort n’est pas, et quand la mort est là, nous ne sommes plus.' Clique sur le bouton pour vivre pleinement la phrase d'Épicure...",
+  "Sartre écrit dans l’Être et le Néant : 'La mort est mon absolu : elle ne peut être dépassée et elle m'appartient en propre. Elle est donc mon ultime possibilité.' Il t'appartient de cliquer une deuxième - ou dernière - fois sur le bouton...",
+];
+
+let counter = 6;
+let bulletIndex = Math.floor(Math.random() * counter);
+const button = document.querySelector("#button");
+const text = document.querySelector("#text");
+const barrel = document.getElementsByClassName("dot");
+
+const pullTheTrigger = () => {
+  counter--;
+  const clic = counter === bulletIndex;
+  if (clic) {
+    console.log("💥");
+    console.log("TU ÊTES MORT");
+    barrel[counter].classList.add("loaded");
+    button.disabled = true;
+    text.textContent = "💀💀💀💀💀💀💀💀💀";
+  } else if (counter === 5) {
+    console.log(counter);
+    console.log(quotes[4]);
+    barrel[5].classList.add("empty");
+    text.textContent = quotes[4];
+  } else if (counter === 4) {
+    console.log(counter);
+    console.log(quotes[3]);
+    barrel[4].classList.add("empty");
+    text.textContent = quotes[3];
+  } else if (counter === 3) {
+    console.log(counter);
+    console.log(quotes[2]);
+    barrel[3].classList.add("empty");
+    text.textContent = quotes[2];
+  } else if (counter === 2) {
+    console.log(counter);
+    console.log(quotes[1]);
+    barrel[2].classList.add("empty");
+    text.textContent = quotes[1];
+  } else if (counter === 1) {
+    console.log(counter);
+    console.log(quotes[0]);
+    barrel[1].classList.add("empty");
+    text.textContent = quotes[0];
+  }
+};
+
+button.addEventListener("click", pullTheTrigger);
