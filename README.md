03d7ea232a2aa75aa1bbe29d6a95787342549b5c
mguittari <mattias.guittari@gmail.com>
2024-12-08 12:17:11 +0100

    footer fixed

--- README.md
+++ README.md
@@ -3,14 +3,15 @@
 ### V1
 
 - 1 joueur (même s'il est techniquement possible de jouer à plusieurs)
-- Barillet à 6 emplacements
+- Barillet à 6 chambres
 - 1 seule balle dans le barillet
 - Au clique du bouton, une nouvelle phrase apparait
 - Ronds remplis ou vides qui symbolisent l'état actuel du barillet et se mettent à jour à chaque clique
-- Le joueur clique jusqu'à mourir
+- Le joueur clique jusqu'à "mourir" (il perd alors la partie)
 - Un émoji explosion apparait pendant 1 fraction de seconde au moment de la mort
 - "Vous êtes mort" s'affiche dans un style graphique Dark Souls
 - Bouton relancer le jeu
+- Responsive
 
 ### V2
 
@@ -24,5 +25,5 @@
 
 ### V4
 
-- Choix entre plusieurs taille de barillet (6 - 7 - 8 - 9)
+- Choix entre plusieurs taille de barillet (6 - 7 - 8 - 9 chambres)
 - Possibilité de mettre plus d'une balle dans le barillet (max = capacité max du barillet - 1)
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const quotes = [
   "Pour finir, une citation de Jankélévitch : 'La mort, c’est ce qui met fin à tout, même à la possibilité de la parole.' La dernière chambre du barillet contient la balle. Pose ce revolver et reviens avec tes amis pour la V2.",
-  "Montaigne n'a-t-il pas écrit : 'La mort, c’est l’oubli où l’on est plongé dans le néant, sans espoir de retour.' Plus tu avances dans le jeu et plus l'espoir de retour se fait mince... Veux-tu vraiment continuer ?",
-  "'La mort est notre plus grande ennemie. Mais il faut la regarder en face, car c’est dans le regard de la mort que l’homme apprend à vivre.' Cette citation provient de Nietzsche, dans Ainsi parlait Zarathoustra. Nietzsche aurait-il aimé joué à ce jeu ? Clique sur le bouton pour lui demander...",
+  "Montaigne n'a-t-il pas écrit : 'La mort, c’est l’oubli où l’on est plongé dans le néant, sans espoir de retour.' Plus tu avances dans le jeu et plus l'espoir de retour se fait mince... Veux-tu vraiment continuer\u00A0?",
+  "'La mort est notre plus grande ennemie. Mais il faut la regarder en face, car c’est dans le regard de la mort que l’homme apprend à vivre.' Cette citation provient de Nietzsche, dans Ainsi parlait Zarathoustra. Nietzsche aurait-il aimé joué à ce jeu\u00A0? Clique sur le bouton pour lui demander...",
   "Épicure dans la lettre à Ménécée : 'La mort n’est rien pour nous, puisque, tant que nous existons, la mort n’est pas, et quand la mort est là, nous ne sommes plus.' Clique sur le bouton pour vivre pleinement la phrase d'Épicure...",
   "Sartre écrit dans l’Être et le Néant : 'La mort est mon absolu : elle ne peut être dépassée et elle m'appartient en propre. Elle est donc mon ultime possibilité.' Il t'appartient de cliquer une deuxième - ou dernière - fois sur le bouton...",
 ];
